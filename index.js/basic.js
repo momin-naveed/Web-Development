c8aa10efa61a0d0e656680ab94216592c8996656
mominnaveed <mominnaveed220@gmail.com>
2021-11-26 04:21:03 +0800

    javascript start

---
+++ index.js/basic.js
@@ -0,0 +1,5 @@
+alert(prompt("Compose your tweet:").slice(0, 140));
+// var tweetCount = tweet.length;
+// alert("you have written "+ tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining.")
+// var name = "Momin";
+// name.slice(2,5);
