805498311129a9c64e90d14ce251edb2c0c6e653
mominnaveed <mominnaveed220@gmail.com>
2021-11-27 05:35:23 +0800

    your message

--- index.js/basic.js
+++ index.js/basic.js
@@ -1,5 +1,10 @@
 alert(prompt("Compose your tweet:").slice(0, 140));
 // var tweetCount = tweet.length;
 // alert("you have written "+ tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining.")
+
 // var name = "Momin";
 // name.slice(2,5);
+
+// var dogAge = prompt("What is your dogAge")
+// var humanAge = ((dogAge - 2) * 4) + 21;
+// alert("Your dog is " + humanAge + " years old in human years.");
