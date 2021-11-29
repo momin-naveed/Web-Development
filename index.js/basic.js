3884bc1c3a22d203bf4f7567200db77573b87575
mominnaveed <mominnaveed220@gmail.com>
2021-11-29 15:33:32 +0800

    Website

--- index.js/basic.js
+++ index.js/basic.js
@@ -8,3 +8,16 @@ alert(prompt("Compose your tweet:").slice(0, 140));
 // var dogAge = prompt("What is your dogAge")
 // var humanAge = ((dogAge - 2) * 4) + 21;
 // alert("Your dog is " + humanAge + " years old in human years.");
+function getMilk(money, costPerBottle) {
+    console.log("buy " + calcBottles(money, 2) + " bottles of milk");
+    return money % 2;
+}
+function calcBottles(startingMoney, costPerBottle) {
+    var numberOfBottles = Math.floor(startingMoney / costPerBottle);
+    return numberOfBottles;
+}
+function calcChange(startingAmount, costPerBottle) {
+    var change = startingAmount % costPerBottle;
+    return change;
+}
+console.log("Hello master, here is your " + getMilk(10, 2) + " change.");
