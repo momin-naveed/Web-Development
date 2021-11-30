13f15f62673d5447d3e6697cdc1e96f8f6deed5c
mominnaveed <mominnaveed220@gmail.com>
2021-11-30 14:00:28 +0800

    index2.js

---
+++ index2.js
@@ -0,0 +1,8 @@
+// you can use + in front of a string to covert it into a number
+console.log(typeof + "5")
+// + in front of data object will convert it into number of milliseconds
+console.log(+new Date())
+// | 0 after a floating point number will convert in into a integer
+console.log(35.354 | 0)
+// use es6 to remove duplicate from array
+console.log([...new Set([1, 2, 2, 3, 4, 4, 5])])
