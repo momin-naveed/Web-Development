6fb4771805103c974b99358b60730628900c9780
mominnaveed <mominnaveed220@gmail.com>
2021-12-04 21:08:50 +0500

    Lunch Solution

---
+++ Buying.js
@@ -0,0 +1,9 @@
+function whosPaying(names) {
+    var numberOfPeople = names.length;
+    var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+    var randomPerson = names[randomPersonPosition];
+    return randomPerson + " is going to lunch today!"
+}
+console.log(whosPaying(["Momin", "Adeel", "Saim", "Zohaib", "Wahab"]));
+
+// whosPaying("adeel")
